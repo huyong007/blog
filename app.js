b6d273aad3818184ee023ad89bcdbbacdcdcc92b
huyong <18168400272@163.com>
2018-05-16 12:44:15 +0800

    morgan和function对比

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 
 
-require('./models/init');
+//require('./models/init');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-
+var fs = require('fs')
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 
@@ -17,6 +17,11 @@ var api = require('./route.api');
 var page = require('./route.page');
 
 var app = express();
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+// setup the logger
+app.use(logger('short', {stream: accessLogStream}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,11 +39,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //这里可以加上各种的中间件
 
-app.use('/',page);
-app.use('/api', api);
+app.use('/', function (req, res, next) {
+  console.log("process one is " + ' ' + req.path);
+  next();
+}, page);
+app.use('/api',function (req, res, next) {
+  console.log("process two is " + ' ' + req.path);
+  next();
+}, api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -47,7 +58,7 @@ app.use(function(req, res, next) {
 var debug = require('debug')('blog:app');
 debug('test app log');
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
