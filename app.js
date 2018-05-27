32ac68dffa6973deef59bf7a1e57a9f0997272ce
huyong <18168400272@163.com>
2018-05-27 17:32:15 +0800

    实验express-session

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var cookieParser = require('cookie-parser');
 var fs = require('fs')
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 
 
@@ -51,7 +52,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //这里可以加上各种的中间件
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
+app.use(function (req, res, next) {
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+  next()
+})
+
+app.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+app.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
 
 
 app.use('/', function (req, res, next) {
