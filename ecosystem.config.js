2fb8393c5e13ddaac18694bd459cdb32a10e3e09
huyong <18168400272@163.com>
2018-07-19 00:57:49 +0800

    change ssh_options

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:huyong007/blog-express.git',
       path: '/www/website/production',
+      ssh_options: ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       'post-deploy': 'yarn && pm2 reload ecosystem.config.js --env dev',
       env: {
         NODE_ENV: 'dev'
